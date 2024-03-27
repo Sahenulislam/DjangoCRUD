f1e6edd18bd3b79596da911bac039d7c036a9d6d
sahenulislam <sahenulislam02@gmail.com>
2024-03-27 12:15:50 +0600

    create update section

--- StudentForm/urls.py
+++ StudentForm/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('create/', views.Student_Form, name='student_form'),
     path('read/', views.student_list, name='student_list'),
     path('update/<str:id>/', views.update_student, name='update_student'),
+    path('delete/<str:id>/', views.delete_student, name='delete_student'),
 
 ]
--- StudentForm/views.py
+++ StudentForm/views.py
@@ -2,7 +2,8 @@
 from django.shortcuts import render
 from .forms import StudentForm
 from .models import Student
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404,HttpResponse
+from django.http import HttpResponseNotAllowed
 
 
 def Student_Form(request):
@@ -10,13 +11,17 @@ def Student_Form(request):
         form = StudentForm(request.POST)
         if form.is_valid():
             student_id = form.cleaned_data['student_id']
-            student_name = form.cleaned_data['student_name']
-            student_phone = form.cleaned_data['student_phone']
-            form.save()
-            return render(request, 'success.html', {'student_name': student_name, 'student_phone': student_phone})
+            if Student.objects.filter(student_id=student_id).exists():
+                return HttpResponse('Student with this ID already exists.')
+            else:
+                student_name = form.cleaned_data['student_name']
+                student_phone = form.cleaned_data['student_phone']
+                form.save()
+                return render(request, 'student_list.html', {'student_name': student_name, 'student_phone': student_phone})
     else:
         form = StudentForm()
     return render(request, 'StudentForm.html', {'form': form})
+
 
 
 def student_list(request):
@@ -34,3 +39,16 @@ def update_student(request, id):
     else:
         form =  StudentForm(instance=student)
     return render(request, 'update_student.html', {'form': form, 'student': student})
+
+
+def delete_student(request, id):
+    if request.method == 'GET':
+        # Retrieve all students with the specified ID
+        students = Student.objects.filter(student_id=id)
+        # Delete all retrieved students
+        students.delete()
+        return redirect('student_list')
+    else:
+        return HttpResponseNotAllowed(['GET'])
+
+
